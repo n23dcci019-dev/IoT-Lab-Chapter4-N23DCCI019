7afde112976eef7043894433fcbbc95fc333b1d6
Le Khanh Duy <n23dcci019@student.ptithcm,edu.vn>
2026-04-21 03:52:57 +0700

    Fix: loi khoang trang va do dai dong flake8

--- BT1_QEMU_SystemMonitor/system_monitor.py
+++ BT1_QEMU_SystemMonitor/system_monitor.py
@@ -22,11 +22,11 @@ try:
         ram_total_mb = ram.total // (1024**2)
         ram_pct = ram.percent
 
-        disk  = psutil.disk_usage('/')
+        disk = psutil.disk_usage('/')
         disk_pct = disk.percent
 
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        line =f'[{now}] CPU: {cpu_avg:.1f}% | RAM: {ram_used_mb}/{ram_total_mb} MB ({ram_pct}%) | Disk: {disk_pct}%|{status}'
+        line = f'[{now}] CPU:{cpu_avg:.1f}% | RAM:{ram_used_mb}/{ram_total_mb}MB ({ram_pct}%) | Disk:{disk_pct}%|{status}'
         print(line)
 
         if status != 'NORMAL':
